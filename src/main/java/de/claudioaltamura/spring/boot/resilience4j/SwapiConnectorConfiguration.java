688cbe296afd722ded80bc56a27f7ff4339b329d
Claudio Altamura <claudio@claudioaltamura.de>
2024-06-21 16:33:51 +0200

    chore: fixed warning

--- src/main/java/de/claudioaltamura/spring/boot/resilience4j/SwapiConnectorConfiguration.java
+++ src/main/java/de/claudioaltamura/spring/boot/resilience4j/SwapiConnectorConfiguration.java
@@ -1,5 +1,6 @@
 package de.claudioaltamura.spring.boot.resilience4j;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +12,7 @@ public class SwapiConnectorConfiguration {
     @Value("${swapiConnector.baseURL}")
     private String baseURL;
 
+    @Autowired
     public SwapiConnectorConfiguration() {}
 
     public SwapiConnectorConfiguration(String baseURL) {
